b1e77c50bb9a6ce54e1d476f09d3dd5abff47bd6
Marc Brinkmann <git@marcbrinkmann.de>
2016-02-25 15:05:49 +0100

    Docs for root module.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,14 @@
 //! Read/Write traits for reading common formats like length-prefixed packets.
+//!
+//! `rwutils` handles a few common formats of binary data:
+//!
+//! * C-style strings, that is 0-terminated sequences of non-zero bytes inside
+//!   the `bytes0` module. Check its docs for examples of reading Strings.
+//! * Length prefixed packets, a u8/u16/u32/u64 of the number of bytes to
+//!   read, followed by the actual data. See `length_prefix`.
+//!
+//! Please read the notes regarding security in module as well, especially if
+//! you are handling untrusted user input.
 
 extern crate byteorder;
 mod cstring;
