74ad65d6cd30c80d62fe3d2ce2b6b51288b7e0be
Marc Brinkmann <git@marcbrinkmann.de>
2016-02-25 16:08:40 +0100

    bytes0 docs.

--- src/bytes0.rs
+++ src/bytes0.rs
@@ -1,3 +1,26 @@
+//! Read and write 0-terminated byte strings.
+//!
+//! Example:
+//!
+//! ```
+//! use rwutil::bytes0::ReadBytes0Ext;
+//!
+//! // a &[u8] implements the io::Read interface
+//! let src: Vec<u8> = vec![0x61, 0x62, 0x63, 0];
+//!
+//! // reading data into a new vector
+//! let buf: Vec<u8> = src.as_slice().read_bytes0().unwrap();
+//!
+//! assert_eq!(buf, b"abc")
+//! ```
+//!
+//! # Security
+//! The current implementation is similar to the trouble `gets` from C in that
+//! it does not have a maximum size for reading. While you will not encounter
+//! a buffer overflow, the vector the string is read into can grow arbitrarily
+//! large, allow a supplier of untrusted data to cause an OOM panic if allowed
+//! to send an infinite amount of data.
+
 use std::io;
 use std::io::{Read, Write};
 
