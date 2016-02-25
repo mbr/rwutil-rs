9001fedee9e8d1d34a512e2b1743cdf3bd03e3f5
Marc Brinkmann <git@marcbrinkmann.de>
2016-02-25 02:21:32 +0100

    Added u32 length prefixed writes.

--- src/length_prefixed.rs
+++ src/length_prefixed.rs
@@ -3,19 +3,39 @@
 //! Implements a popular format for sending data packets: First, a 32-bit
 //! integer in big endian byte order is sent with the length of the data,
 //! followed by the actual data.
-use byteorder::{BigEndian, WriteBytesExt};
+use byteorder::{BigEndian, WriteBytesExt, ByteOrder};
 use std::io;
+
+use std::vec::Vec;
 
 /// Allow sending of byte-slices with length prefix.
 pub trait LengthWriteExt : io::Write {
-    fn send_length_prefixed(&mut self, data: &[u8]) -> io::Result<()>;
+    fn write_prefixed<T: ByteOrder>(&mut self, data: &[u8]) ->
+    io::Result< ()>;
 }
 
 impl<W: io::Write> LengthWriteExt for W {
-    fn send_length_prefixed(&mut self, data: &[u8]) -> io::Result<()> {
-        try!(self.write_u32::<BigEndian>(data.len() as u32));
+    fn write_prefixed<T: ByteOrder>(&mut self, data: &[u8]) ->
+    io::Result< ()> {
+        try!(self.write_u32::<T>(data.len() as u32));
         try!(self.write_all(&data));
 
         Ok(())
+    }
+}
+
+mod test {
+    use std::io::Write;
+    use length_prefixed::LengthWriteExt;
+    use byteorder::BigEndian;
+
+    #[test]
+    fn test_u32_write() {
+        let mut buf = Vec::new();
+        let expected = vec![0, 0, 0, 5, 0x61, 0x62, 0x63, 0x64, 0x65];
+        //buf.write_prefixed();
+        buf.write_prefixed::<BigEndian>(b"abcde");
+
+        assert_eq!(&expected, &buf);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,2 @@
-#[cfg(test)]
-mod test;
+extern crate byteorder;
+mod length_prefixed;
