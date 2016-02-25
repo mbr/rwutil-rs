f939c4f0e60b218c5db5b46b0ca8dd84d9f302e6
Marc Brinkmann <git@marcbrinkmann.de>
2016-02-25 01:57:54 +0100

    Initial import.

---
+++ src/length_prefixed.rs
@@ -0,0 +1,21 @@
+//! Length-prefixed stream packets
+//!
+//! Implements a popular format for sending data packets: First, a 32-bit
+//! integer in big endian byte order is sent with the length of the data,
+//! followed by the actual data.
+use byteorder::{BigEndian, WriteBytesExt};
+use std::io;
+
+/// Allow sending of byte-slices with length prefix.
+pub trait LengthWriteExt : io::Write {
+    fn send_length_prefixed(&mut self, data: &[u8]) -> io::Result<()>;
+}
+
+impl<W: io::Write> LengthWriteExt for W {
+    fn send_length_prefixed(&mut self, data: &[u8]) -> io::Result<()> {
+        try!(self.write_u32::<BigEndian>(data.len() as u32));
+        try!(self.write(&data));
+
+        Ok(())
+    }
+}
