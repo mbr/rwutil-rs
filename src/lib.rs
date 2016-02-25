9c10494881dc8b9ccca40eb13eaf061cb44e629f
Marc Brinkmann <git@marcbrinkmann.de>
2016-02-25 15:10:56 +0100

    More docs on root.

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,12 @@
 //! * Length prefixed packets, a u8/u16/u32/u64 of the number of bytes to
 //!   read, followed by the actual data. See `length_prefix`.
 //!
-//! Please read the notes regarding security in module as well, especially if
-//! you are handling untrusted user input.
+//! All readers are implemented as additional traits for the `io::Read` and
+//! `io::Write`. After using them, additional methods become available on these
+//! types.
+//!
+//! **Please read the notes regarding security** in module as well,
+//! especially if you are handling untrusted user input.
 
 extern crate byteorder;
 mod cstring;
